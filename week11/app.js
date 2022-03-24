56d793275bb739f6feca0bd1afb448ce28e6acb0
Garcia <egarcia4@FVFY36RHJ1WK.tld>
2022-03-24 11:41:15 -0500

    Completely finished with Week 11

--- week11/app.js
+++ week11/app.js
@@ -1,128 +1,115 @@
+function askName() {
+    var name = prompt("Enter your name");
+    return name
+}
 
-function playAgain() {
+
+function greeting(a) {
+    alert(`Welcome, ${a}! I hope you are well.`);
+}
+
+function humanTurn() {
+    var hum =0;
+    do {
+        var ran2 = Math.floor(Math.random() * 10) +1
+
+        console.log(ran2);
+
+        hum = hum + ran2
+
+        console.log(hum);
+
+        var humnum = prompt(`Your current number is ${hum}. \n Would you like another number? (y or n)  \n If you go over 20, you will lose`); 
+
+        if (hum >= 21){
+            humnum = "n";
+        }
+
+    } while (humnum != "n") 
+
+    
+    alert (`Your final number is ${hum}`);
+    
+
+    return hum
+}
+
+function computerTurn(){
+    var comp = 0;
 
     do {
 
-    function askName() {
-        var name = prompt("Enter your name");
-        return name
-    }
+        var ran = Math.floor(Math.random() * 10) +1
 
+        console.log(ran);
 
+        comp = comp + ran
 
-    function greeting(a) {
-        alert(`Welcome, ${a}! I hope you are well.`);
-    }
-
-
-
-
-
-
-
-    function humanTurn() {
-        var hum =0;
-        do {
-            var ran2 = Math.floor(Math.random() * 10) +1
-
-            console.log(ran2);
-
-            hum = hum + ran2
-
-            console.log(hum);
-
-            var humnum = prompt(`Your current number is ${hum}. \n Would you like another number? (y or n)  \n If you go over 20, you will lose`); 
-
-            if (hum >= 21){
-                humnum = "n";
-            }
-
-        } while (humnum != "n") 
-
-
-        
-        alert (`Your final number is ${hum}`);
-        
-
-
-        return hum
-    }
-
-
-
-    function computerTurn(){
-        var comp = 0;
-
-        do {
-
-            var ran = Math.floor(Math.random() * 10) +1
-
-            console.log(ran);
-
-            comp = comp + ran
-
-            console.log(comp)
-        
-
-        } while (comp <= 16) {
-
-            alert(`My number is ${comp}`)
-
-        }
-
+        console.log(comp)
     
-            alert ("Lets tally up")
-            return comp
+
+    } while (comp <= 16) {
+
+        alert(`My number is ${comp}`)
+
     }
 
 
+    alert ("Lets tally up")
+    return comp
+}
 
-    function compareNumbers(name, hum, comp ) {
-
-        var h = 0;
-        var c = 0;
+function compareNumbers(name, hum, comp ) {
 
 
+    if (hum > 20){
 
-        if (hum > 20){
+        alert(`Your number was ${hum}. Since you went over 20, I win. `);
+        c++
 
-            alert(`Your number was ${hum}. Since you went over 20, I win. `);
-            c++
+    }else if (comp > 20) {
+        
+        alert(`My number was ${comp}. Since I went over 20, you win. `);
+        h++
 
-        }else if (comp > 20) {
-            
-            alert(`My number was ${comp}. Since I went over 20, you win. `);
-            h++
+    }else if (hum > comp) {
 
-        }else if (20 >= hum > comp) {
+        alert(`The score was Human: ${hum} to Computer: ${comp}. You (human) win. `);
+        h++
 
-            alert(`The score was Human: ${hum} to Computer: ${comp}. You win. `);
-            h++
+    } else if (hum < comp) {
 
-        } else if (hum < comp <= 20) {
+        alert(`The score was Human: ${hum} to Computer: ${comp}. I (computer) win. `);
+        c++
 
-            alert(`The score was Human: ${hum} to Computer: ${comp}. I win. `);
-            c++
+    } else if (hum == comp) {
 
-        } else if (hum == comp) {
+        alert(`The score was Human: ${hum} to Computer: ${comp}. We tied. Both get points. `);
+        c++
+        h++
 
-            alert(`The score was Human: ${hum} to Computer: ${comp}. We tied. Both get points. `);
-            c++
-            h++
-
-        }
-        alert(`Final score was Human: ${h} to Computer: ${c}`)
-        return h,c 
     }
+    alert(`Final score was Human: ${h} to Computer: ${c}`)
+    return h,c 
+}
+
+
+function playAgain() {
 
     compareNumbers(greeting(askName()),humanTurn(),computerTurn())
-
     var leave = prompt("Would you like to keep playing? (yes or no)");
-
-} while (leave != "no") 
-
-alert("Have a good day")
+    if (leave != "no") {
+        playAgain() // recursive function
+    } else {
+        alert(`Have a good day. Final score was Human: ${h} to Computer: ${c} `)
+    }
 
 }
 
+var h = 0;
+var c = 0;
 playAgain()
+
+
+
+
